f30ef8174aa0b9c25c55c29fa99b60cec33355d2
Your Name <you@example.com>
2024-03-03 16:00:10 -0500

    change

--- home.nix
+++ home.nix
@@ -97,7 +97,6 @@
       color-scheme = "prefer-dark";
       #gtk-theme = "Nightfox-Dusk-BL-LB";
       gtk-theme = "Tokyonight-Dark-BL-LB";
-      cursor-theme = "material_cursors";
     };
   };
 }
