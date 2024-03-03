fe15b40c30a9a12d12fbd5980b4ff72aa3d8059f
Your Name <you@example.com>
2024-03-03 12:16:33 -0500

    change

--- home.nix
+++ home.nix
@@ -88,32 +88,4 @@
     url = "https://github.com/newmanls/rofi-themes-collection.git";
     rev = "5bc150394bf785b2751711e3050ca425c662938e";
   } + "/themes";  
-
-  # gtk themes
-
-home.pointerCursor = {
-  gtk.enable = true;
-  # x11.enable = true;
-  package = pkgs.bibata-cursors;
-  name = "Bibata-Modern-Classic";
-  size = 16;
-};
-
-gtk = {
-  enable = true;
-  theme = {
-    package = pkgs.flat-remix-gtk;
-    name = "Flat-Remix-GTK-Grey-Darkest";
-  };
-
-  iconTheme = {
-    package = pkgs.gnome.adwaita-icon-theme;
-    name = "Adwaita";
-  };
-
-  font = {
-    name = "Sans";
-    size = 11;
-  };
-};
 }
