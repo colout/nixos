7b9db06bec723796260fe7342e40c0b50070045e
Your Name <you@example.com>
2024-03-03 12:18:01 -0500

    change

--- modules/wm/hyprland.nix
+++ modules/wm/hyprland.nix
@@ -62,5 +62,27 @@
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
+    home.pointerCursor = {
+    package = pkgs.catppuccin-cursors.mochaPeach;
+    name = "Catppuccin-Mocha-Peach-Cursors";
+    size = 40;
+    gtk.enable = true;
+  };
 
+  gtk = {
+    enable = true;
+    font = {
+      package = (pkgs.nerdfonts.override { fonts = [ "Mononoki" ]; });
+      name = "Mononoki Nerd Font Regular";
+      size = 18;
+    };
+    iconTheme = {
+      package = (pkgs.catppuccin-papirus-folders.override { flavor = "mocha"; accent = "peach"; });
+      name  = "Papirus-Dark";
+    };
+    theme = {
+      package = (pkgs.catppuccin-gtk.override { accents = [ "peach" ]; size = "standard"; variant = "mocha"; });
+      name = "Catppuccin-Mocha-Standard-Peach-Dark";
+    };
+  };
 }
