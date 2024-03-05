8a8a3d9e85acfc6fe010340e75483648e39293ff
Your Name <you@example.com>
2024-03-04 20:35:30 -0500

    change

--- modules/games.nix
+++ modules/games.nix
@@ -6,7 +6,7 @@
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     }; 
-    gamemods = {
+    gamemode = {
       enable = true;
       enableRenice = true;
     };
