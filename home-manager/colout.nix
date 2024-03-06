f1874e68e62c36c2cac21ee3244c6c0c969d0cd4
Your Name <you@example.com>
2024-03-06 18:24:53 -0500

    change

--- home-manager/colout.nix
+++ home-manager/colout.nix
@@ -6,6 +6,7 @@
   imports = [
     ../../modules/home-manager/zsh.nix
   ];
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "colout";
@@ -57,30 +58,6 @@
     mc
     floorp
   ];
-
-  programs.zsh = {
-    enable = true;
-    zplug = {
-      enable = true;
-      plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; }
-        { name = "zsh-users/zsh-completions"; }
-        { name = "zsh-users/zsh-autosuggestions"; }
-        { name = "zsh-users/zsh-syntax-highlighting"; }
-        { name = "themes/robbyrussell"; tags = [ as:theme from:oh-my-zsh ]; }
-        { name = "plugins/dirhistory"; tags = [ from:oh-my-zsh ]; }
-      ];
-    };
-    shellAliases = {
-      ll = "ls -l";
-
-      # NixOS command shortcuts
-      napply = "sudo chown -R $USER /etc/nixos; cd /etc/nixos && git -C /etc/nixos add . && git -C /etc/nixos commit -m 'change'; sudo nixos-rebuild switch --flake /etc/nixos#default";
-      nupdate = "nix flake update /etc/nixos";
-      nedit = "cd /etc/nixos; nvim";
-      ngarbage-collect = "nix-collect-garbage -d";
-    };
-  };
   programs.kitty = {
     enable = true;
     settings = {
---
+++ modules/home-manager/zsh.nix
@@ -0,0 +1,25 @@
+{
+  programs.zsh = {
+    enable = true;
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; }
+        { name = "zsh-users/zsh-completions"; }
+        { name = "zsh-users/zsh-autosuggestions"; }
+        { name = "zsh-users/zsh-syntax-highlighting"; }
+        { name = "themes/robbyrussell"; tags = [ as:theme from:oh-my-zsh ]; }
+        { name = "plugins/dirhistory"; tags = [ from:oh-my-zsh ]; }
+      ];
+    };
+    shellAliases = {
+      ll = "ls -l";
+
+      # NixOS command shortcuts
+      napply = "sudo chown -R $USER /etc/nixos; cd /etc/nixos && git -C /etc/nixos add . && git -C /etc/nixos commit -m 'change'; sudo nixos-rebuild switch --flake /etc/nixos#default";
+      nupdate = "nix flake update /etc/nixos";
+      nedit = "cd /etc/nixos; nvim";
+      ngarbage-collect = "nix-collect-garbage -d";
+    };
+  };
+}
