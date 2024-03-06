f13d05880c9e6480d27d113a70946eb766de0bb6
Your Name <you@example.com>
2024-03-06 15:54:10 -0500

    change

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -33,7 +33,6 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  networking.hostName = "xiangbing";
   # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp7s0.useDHCP = lib.mkDefault true;
