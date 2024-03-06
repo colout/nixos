a1b7adadcd5bd51c111acd7075d8d8ad71f20b64
Your Name <you@example.com>
2024-03-06 18:40:20 -0500

    change

--- home-manager/colout.nix
+++ home-manager/colout.nix
@@ -4,7 +4,7 @@
   nixpkgs.config.allowUnfreePredicate = _: true; 
 
   imports = [
-    ../../modules/home-manager/zsh.nix
+    ./modules/zsh.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
