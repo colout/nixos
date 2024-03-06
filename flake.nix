7dd59d33c3d831c1cef89c8b0476865bcb12c16e
Your Name <you@example.com>
2024-03-06 16:10:00 -0500

    change

--- flake.nix
+++ flake.nix
@@ -52,7 +52,6 @@ outputs = { self, nixpkgs, hyprland, ... }@inputs:
     # Home-manager configs
     homeConfigurations = {
       "colout@xiangbing" = home-manager.lib.homeConfigurations {
-      }
-    }
+    };
   };
 }
