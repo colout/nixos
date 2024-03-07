285cb3fbfb2dac7087bc8efe9a61ad992552f936
Your Name <you@example.com>
2024-03-06 20:14:13 -0500

    change

--- home-manager/modules/nvim.nix
+++ home-manager/modules/nvim.nix
@@ -1,97 +1,20 @@
-{ config, pkgs, ... }:
-
+{ pkgs, lib, ... }:
 let
-  lspServers = pkgs.writeText "lsp_servers.json" (builtins.toJSON (import ./lsp_servers.nix { inherit pkgs; }));
+  nixvim = import (builtins.fetchGit {
+    url = "https://github.com/nix-community/nixvim";
+    # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+    # ref = "nixos-23.05";
+  });
 in
 {
-  home.sessionVariables = {
-    EDITOR = "${config.home.profileDirectory}/bin/nvim";
-  };
+  imports = [
+    # For home-manager
+    nixvim.homeManagerModules.nixvim
+    # For NixOS
+    nixvim.nixosModules.nixvim
+    # For nix-darwin
+    nixvim.nixDarwinModules.nixvim
+  ];
 
-  programs.neovim = {
-    enable = true;
-    package = pkgs.neovim-nightly.overrideAttrs (_: { CFLAGS = "-O3"; });
-    vimAlias = true;
-    viAlias = true;
-    withNodeJs = true;
-    defaultEditor = true;
-    withPython3 = true;
-    withRuby = false;
-    extraConfig = ''
-      let mapleader=" "
-
-      lua <<EOF
-        require("config.general")
-        require("config.remaps")
-      EOF
-    '';
-    extraPackages = with pkgs; [
-      # Essentials
-      nodePackages.npm
-      nodePackages.neovim
-
-      # Python
-      (python3.withPackages (ps: with ps; [
-        setuptools # Required by pylama for some reason
-        pylama
-        black
-        isort
-        yamllint
-        debugpy
-      ]))
-      nodePackages.pyright
-
-      # Lua
-      unstable.lua-language-server
-      selene
-
-      # Nix
-      statix
-      nixpkgs-fmt
-      nil
-
-      # C, C++
-      clang-tools
-      cppcheck
-
-      # Shell scripting
-      shfmt
-      shellcheck
-      shellharden
-
-      # JavaScript
-      nodePackages.prettier
-      nodePackages.eslint
-      nodePackages.typescript-language-server
-
-      # Go
-      go
-      gopls
-      golangci-lint
-      delve
-
-      # Additional
-      nodePackages.bash-language-server
-      nodePackages.yaml-language-server
-      nodePackages.dockerfile-language-server-nodejs
-      nodePackages.vscode-langservers-extracted
-      nodePackages.markdownlint-cli
-      taplo-cli
-      codespell
-      gitlint
-      terraform-ls
-      actionlint
-
-      # Telescope dependencies
-      ripgrep
-      fd
-    ];
-  };
-
-  home.file = {
-    ".config/nvim" = {
-      recursive = true;
-      source = ../../dotfiles/nvim;
-    };
-  };
+  programs.nixvim.enable = true;
 }
