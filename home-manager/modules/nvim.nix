a89aec936926b43e4d91d374026c4d4aa7743198
Your Name <you@example.com>
2024-03-06 22:16:18 -0500

    change

--- home-manager/modules/nvim.nix
+++ home-manager/modules/nvim.nix
@@ -13,13 +13,6 @@
       style = "storm"; # The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
       transparent = true; # Enable this to disable setting the background color
       styles = {
-        # Style to be applied to different syntax groups
-        # Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = "{ italic = true }";
-        keywords = "{ italic = true }";
-        functions = "{}";
-        variables = "{}";
-        # Background styles. Can be "dark"; "transparent" or "normal"
         sidebars = "transparent"; # style for sidebars, see below
         floats = "transparent"; # style for floating windows
       };
