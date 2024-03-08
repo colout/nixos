f20df97af598386d2a5aef6dbe9231e7a9981218
Your Name <you@example.com>
2024-03-08 13:56:14 -0500

    change

--- home-manager/modules/nvim/plugins/lsp/nvim-cmp.nix
+++ home-manager/modules/nvim/plugins/lsp/nvim-cmp.nix
@@ -8,7 +8,7 @@
       sources = [
         {name = "path";}
         {name = "nvim_lsp";}
-        {name = "luasnip";}
+        #{name = "luasnip";}
         {name = "crates";}
         {name = "buffer";}
       ];
