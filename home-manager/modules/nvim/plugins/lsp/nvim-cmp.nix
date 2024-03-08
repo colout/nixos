d8bfd6006948d1a1721fa74a3adff3fea4790939
Your Name <you@example.com>
2024-03-08 15:01:50 -0500

    change

--- home-manager/modules/nvim/plugins/lsp/nvim-cmp.nix
+++ home-manager/modules/nvim/plugins/lsp/nvim-cmp.nix
@@ -5,6 +5,13 @@
       cmp = {
         enable = true;
 
+        sources = [
+          {name = "path";}
+          {name = "nvim_lsp";}
+          {name = "luasnip";}
+          {name = "crates";}
+          {name = "buffer";}
+        ];
         # It's annoying to have to `shift+tab` up to what I want to select, these
         # settings stop `cmp` from starting in the middle of the list.
         settings = {
