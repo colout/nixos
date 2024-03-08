94d9d094e74ee71a682fb8d0906a83711995e92a
Your Name <you@example.com>
2024-03-08 11:51:32 -0500

    change

--- home-manager/modules/nvim/visuals/lualine/default.nix
+++ home-manager/modules/nvim/visuals/lualine/default.nix
@@ -26,35 +26,49 @@
 
         theme = let
           colors = {
-            aqua = "#8ec07c";
-            black = "#282828";
-            blue = "#83a598";
-            darkgray = "#3c3836";
-            gray = "#a89984";
-            green = "#b8bb26";
-            inactivegray = "#7c6f64";
-            lightgray = "#504945";
-            red = "#fb4934";
-            transparent = "none";
-            white = "#ebdbb2";
-            yellow = "#fe8019";
+            white = "#ffffff";
+            darkestgreen = "#005f00";
+            brightgreen = "#afdf00";
+            darkestcyan = "#005f5f";
+            mediumcyan = "#87dfff";
+            darkestblue = "#005f87";
+            darkred = "#870000";
+            brightred = "#df0000";
+            brightorange = "#ff8700";
+            gray1 = "#262626";
+            gray2 = "#303030";
+            gray4 = "#585858";
+            gray5 = "#606060";
+            gray7 = "#9e9e9e";
+            gray10 = "#f0f0f0";
+            brightpurple = "#dfafdf";
+            darkpurple = "#870087";
           };
         in {
           normal = {
-            a = {
-              bg = colors.transparent;
-              fg = colors.white;
-              gui = "bold";
-            };
-            b = {
-              bg = colors.transparent;
-              fg = colors.yellow;
-            };
-            c = {
-              bg = colors.transparent;
-              fg = colors.gray;
-            };
-          };
+            a = { fg = Colors.mediumcyan, bg = Colors.darkestblue, gui = "bold" },
+            b = { fg = Colors.gray2, bg = Colors.mediumcyan },
+            c = { fg = Colors.gray7, bg = Colors.gray2 },
+          },
+          command = {
+            a = { fg = Colors.brightgreen, bg = Colors.darkestgreen, gui = "bold" },
+            b = { fg = Colors.gray2, bg = Colors.brightgreen },
+          },
+          insert = {
+            a = { fg = Colors.darkestcyan, bg = Colors.white, gui = "bold" },
+            b = { fg = Colors.darkestcyan, bg = Colors.mediumcyan },
+            c = { fg = Colors.mediumcyan, bg = Colors.darkestblue },
+          },
+          visual = {
+            a = { fg = Colors.brightpurple, bg = Colors.darkpurple, gui = "bold" },
+            b = { fg = Colors.darkpurple, bg = Colors.brightpurple },
+          },
+          replace = { a = { fg = Colors.white, bg = Colors.brightred, gui = "bold" } },
+          inactive = {
+            a = { fg = Colors.gray1, bg = Colors.gray5, gui = "bold" },
+            b = { fg = Colors.gray1, bg = Colors.gray5 },
+            c = { bg = Colors.gray1, fg = Colors.gray5 },
+          },
         };
       };
     };
