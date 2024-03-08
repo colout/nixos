04a3e480192c39d26c200818d2b4cc9fa33d76f6
Your Name <you@example.com>
2024-03-08 14:05:55 -0500

    change

--- home-manager/modules/nvim/plugins/lsp/nvim-cmp.nix
+++ home-manager/modules/nvim/plugins/lsp/nvim-cmp.nix
@@ -2,31 +2,21 @@
 {
   programs.nixvim = {
     plugins = {
-    nvim-cmp = {
-      enable = true;
+      cmp = {
+        enable = true;
 
-      # It's annoying to have to `shift+tab` up to what I want to select, these
-      # settings stop `cmp` from starting in the middle of the list.
-      completion.completeopt = "noselect";
-      
-      mapping = {
-        "<C-k>" = "cmp.mapping.select_prev_item()";
-        "<C-j>" = "cmp.mapping.select_next_item()";
-        "<C-Space>" = "cmp.mapping.complete()";
-        "<C-e>" = "cmp.mapping.abort()";
-        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        # It's annoying to have to `shift+tab` up to what I want to select, these
+        # settings stop `cmp` from starting in the middle of the list.
+        completion.completeopt = "noselect";
+        
+        mapping = {
+          "<C-k>" = "cmp.mapping.select_prev_item()";
+          "<C-j>" = "cmp.mapping.select_next_item()";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.abort()";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+        };
       };
     };
-  };
-
-  # I keep accidentally pressing `q` which prevents `cmp` from working because
-  # of the macro recording. I don't use this anyway.
-  keymaps = [
-    {
-      mode = "n";
-      key = "q";
-      action = "<Nop>";
-    }
-  ];
   };
 }
