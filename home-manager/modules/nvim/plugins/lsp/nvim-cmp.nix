bf6b7e8f8fb8c7b9a7fb46b3eaad34e4c00c30c3
Your Name <you@example.com>
2024-03-08 14:02:14 -0500

    change

--- home-manager/modules/nvim/plugins/lsp/nvim-cmp.nix
+++ home-manager/modules/nvim/plugins/lsp/nvim-cmp.nix
@@ -8,7 +8,7 @@
       sources = [
         {name = "path";}
         {name = "nvim_lsp";}
-        #{name = "luasnip";}
+        {name = "luasnip";}
         {name = "crates";}
         {name = "buffer";}
       ];
@@ -19,8 +19,8 @@
       preselect = "None";
 
       mapping = {
-        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+        "<C-k>" = "cmp.mapping.select_prev_item()";
+        "<C-j>" = "cmp.mapping.select_next_item()";
         "<C-Space>" = "cmp.mapping.complete()";
         "<C-e>" = "cmp.mapping.abort()";
         "<CR>" = "cmp.mapping.confirm({ select = true })";
