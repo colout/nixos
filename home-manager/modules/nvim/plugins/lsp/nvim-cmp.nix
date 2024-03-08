28925c0180233c9270cf5e2f381ef09eea90ca61
Your Name <you@example.com>
2024-03-08 14:03:21 -0500

    change

--- home-manager/modules/nvim/plugins/lsp/nvim-cmp.nix
+++ home-manager/modules/nvim/plugins/lsp/nvim-cmp.nix
@@ -8,8 +8,7 @@
       # It's annoying to have to `shift+tab` up to what I want to select, these
       # settings stop `cmp` from starting in the middle of the list.
       completion.completeopt = "noselect";
-      preselect = "None";
-
+      
       mapping = {
         "<C-k>" = "cmp.mapping.select_prev_item()";
         "<C-j>" = "cmp.mapping.select_next_item()";
