6e889bf6cbb80888319a0530da57fe0e460a1677
Your Name <you@example.com>
2024-03-08 10:28:08 -0500

    change

--- home-manager/modules/nvim/default.nix
+++ home-manager/modules/nvim/default.nix
@@ -8,16 +8,6 @@
 
   programs.nixvim = {
     enable = true;
-    colorschemes.tokyonight = { 
-      enable = true;
-
-      style = "storm"; # The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-      transparent = true; # Enable this to disable setting the background color
-      styles = {
-        sidebars = "transparent"; # style for sidebars, see below
-        floats = "transparent"; # style for floating windows
-      };
-    };
 
     globals.mapleader = " ";
 
