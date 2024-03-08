f50d91a469604d9fc8e269ac4de48ab57b858628
Your Name <you@example.com>
2024-03-08 13:53:41 -0500

    change

--- home-manager/modules/nvim/plugins/default.nix
+++ home-manager/modules/nvim/plugins/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./nvim-tree
     ./telescope
+    ./lsp
   ];
 }
---
+++ home-manager/modules/nvim/plugins/lsp/lsp.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+{
+  programs.nixvim = {
+    plugins = {
+      lsp = {
+        enable = true;
+        servers = {
+          lua-ls.enable = true;
+          marksman.enable = true;
+          nil_ls.enable = true;
+          pylsp.enable = true;
+          yamlls.enable = true;
+        };
+      };
+
+      # icons in completin
+      lspkind = {
+        enable = true;
+      };
+      none-ls.enable = true;
+    };
+  }
+}
