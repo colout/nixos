55179c3fad131ae7240c96a283f49d075dbb4c2f
Your Name <you@example.com>
2024-03-08 12:12:41 -0500

    change

--- home-manager/modules/nvim/visuals/lualine/default.nix
+++ home-manager/modules/nvim/visuals/lualine/default.nix
@@ -27,111 +27,92 @@
         };
 
         theme = let
-          Colors = {
-            white = "#ffffff";
-            darkestgreen = "#005f00";
-            brightgreen = "#afdf00";
-            darkestcyan = "#005f5f";
-            mediumcyan = "#87dfff";
-            darkestblue = "#005f87";
-            darkred = "#870000";
-            brightred = "#df0000";
-            brightorange = "#ff8700";
-            gray1 = "#262626";
-            gray2 = "#303030";
-            gray4 = "#585858";
-            gray5 = "#606060";
-            gray7 = "#9e9e9e";
-            gray10 = "#f0f0f0";
-            brightpurple = "#dfafdf";
-            darkpurple = "#870087";
-          };
-        in {
+          in {
           normal = {
             a = {
-              fg = Colors.mediumcyan;
-              bg = Colors.darkestblue;
+              fg = colors.mediumcyan;
+              bg = colors.darkestblue;
               gui = "bold";
             };
 
             b = {
-              fg = Colors.gray2;
-              bg = Colors.mediumcyan;
+              fg = colors.gray2;
+              bg = colors.mediumcyan;
             };
 
             c = {
-              fg = Colors.gray7;
-              bg = Colors.gray2;
+              fg = colors.gray7;
+              bg = colors.gray2;
             };
           };
 
           command = {
             a = {
-              fg = Colors.brightgreen;
-              bg = Colors.darkestgreen;
+              fg = colors.brightgreen;
+              bg = colors.darkestgreen;
               gui = "bold";
             };
 
             b = {
-              fg = Colors.gray2;
-              bg = Colors.brightgreen;
+              fg = colors.gray2;
+              bg = colors.brightgreen;
             };
           };
 
           insert = {
             a = {
-              fg = Colors.darkestcyan;
-              bg = Colors.white;
+              fg = colors.darkestcyan;
+              bg = colors.white;
               gui = "bold";
             };
 
             b = {
-              fg = Colors.darkestcyan;
-              bg = Colors.mediumcyan;
+              fg = colors.darkestcyan;
+              bg = colors.mediumcyan;
             };
 
             c = {
-              fg = Colors.mediumcyan;
-              bg = Colors.darkestblue;
+              fg = colors.mediumcyan;
+              bg = colors.darkestblue;
             };
           };
 
           visual = {
             a = {
-              fg = Colors.brightpurple;
-              bg = Colors.darkpurple;
+              fg = colors.brightpurple;
+              bg = colors.darkpurple;
               gui = "bold";
             };
 
             b = {
-              fg = Colors.darkpurple;
-              bg = Colors.brightpurple;
+              fg = colors.darkpurple;
+              bg = colors.brightpurple;
             };
           };
 
           replace = {
             a = {
-              fg = Colors.white;
-              bg = Colors.brightred;
+              fg = colors.white;
+              bg = colors.brightred;
               gui = "bold";
             };
           };
 
           inactive = {
             a = {
-              fg = Colors.gray1;
-              bg = Colors.gray5;
+              fg = colors.gray1;
+              bg = colors.gray5;
               gui = "bold";
             };
 
             b = {
-              fg = Colors.gray1;
-              bg = Colors.gray5;
+              fg = colors.gray1;
+              bg = colors.gray5;
             };
 
             c = {
-              bg = Colors.gray1;
-              fg = Colors.gray5;
+              bg = colors.gray1;
+              fg = colors.gray5;
             };
           };
         };
