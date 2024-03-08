b515064f429b389f8eada82017e85cfd3e82c443
Your Name <you@example.com>
2024-03-08 12:22:19 -0500

    change

--- home-manager/modules/nvim/plugins/default.nix
+++ home-manager/modules/nvim/plugins/default.nix
@@ -1,2 +1,5 @@
 {
+  import = [
+    ./nvim-tree
+  ];
 }
---
+++ home-manager/modules/nvim/plugins/nvim-tree/default.nix
@@ -0,0 +1,16 @@
+{ pkg, ... }:
+
+{
+  programs.nixvim = {
+    plugins = {
+
+      nvim-tree = {
+        enable = true;
+
+        view = {
+          width = 35;
+        };
+      };
+    };
+  };
+};
