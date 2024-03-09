e7ff1c4755a50222e11769c3c5f9387ecf4c8560
Your Name <you@example.com>
2024-03-09 10:04:00 -0500

    change

--- home-manager/modules/nvim/tabs/bufferline.nix
+++ home-manager/modules/nvim/tabs/bufferline.nix
@@ -4,6 +4,7 @@
   programs.nixvim = {
     plugins.bufferline = {
       enable = true;
+      showCloseIcon = false;
       numbers = "none";
       diagnostics = "nvim_lsp";
       separatorStyle = "slope";
