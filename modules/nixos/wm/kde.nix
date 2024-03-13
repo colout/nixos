d6905d780901f0951778bf269dc94afba13d5b10
Your Name <you@example.com>
2024-03-13 16:24:34 -0400

    change

--- modules/nixos/wm/kde.nix
+++ modules/nixos/wm/kde.nix
@@ -1,16 +1,14 @@
-{ config, pkgs, inputs, ... }:
 {
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.plasma6.enable = true;
-   
+  services.desktopManager.plasma6.enable = true;
+
   # Configure keymap in X11
   services.xserver = {
     xkb.layout = "us";
     xkb.variant = "";
   };
 }
-
