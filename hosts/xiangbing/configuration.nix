bd7111702d97e18ed266b3ceb78f51e6097fd535
Your Name <you@example.com>
2024-03-13 17:08:56 -0400

    change

--- hosts/xiangbing/configuration.nix
+++ hosts/xiangbing/configuration.nix
@@ -12,7 +12,7 @@
 
   # Kernel
   #boot.kernelPackages = pkgs.linuxPackages;
-  boot.kernelPackages = pkgs.stable.linuxPackages_6_5;
+  boot.kernelPackages = pkgs.stable.linuxPackages_6_6;
 
   system.stateVersion = "23.11"; # Did you read the comment?
 
