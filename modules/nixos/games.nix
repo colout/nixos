9abe72bccc92758452ca06ffec897e58de3ac897
Your Name <you@example.com>
2024-03-14 16:57:08 -0400

    change

--- modules/nixos/games.nix
+++ modules/nixos/games.nix
@@ -1,29 +1,23 @@
-{ config, pkgs, inputs, overlays, ... }: {
+{ pkgs, ... }: {
   programs = {
     steam = {
       enable = true;
-      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    }; 
+      remotePlay.openFirewall =
+        true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall =
+        true; # Open ports in the firewall for Source Dedicated Server
+    };
 
     gamemode = {
       enable = true;
       enableRenice = true;
-      settings = {
-        general = {
-         renice = 19;
-        };
-      };
+      settings = { general = { renice = 19; }; };
     };
-  }; 
+  };
 
   hardware.opengl.driSupport32Bit = true;
 
   environment.systemPackages = with pkgs; [
-    (wineWowPackages.waylandFull.override {
-      #wineRelease = "staging";
-      mingwSupport = true;
-    })
     bottles
 
     mangohud
@@ -31,11 +25,11 @@
     discord
     appimage-run
     (lutris.override {
-      extraLibraries =  pkgs: [
+      extraLibraries = pkgs: [
         appimage-run # for rcps3
         fuse
       ];
-    })    
+    })
 
     dwarf-fortress-packages.dwarf-fortress-full
     superTux
