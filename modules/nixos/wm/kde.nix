898ad55e78f37ce710b8c48e8b07f49aedaedf0d
Your Name <you@example.com>
2024-04-22 13:19:21 -0400

    change

--- modules/nixos/wm/kde.nix
+++ modules/nixos/wm/kde.nix
@@ -1,14 +1,17 @@
+{ pkgs, ... }:
+
 {
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "";
+  services = {
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      # Configure keymap in X11
+      xkb.layout = "us";
+      xkb.variant = "";
+      desktopManager.plasma6.enable = true;
+    };
   };
+
+  environment.systemPackages = with pkgs.unstable; [ libsForQt5.ark ];
 }
