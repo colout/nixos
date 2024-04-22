3cb3dac03922ad79e586a0c641952461f8a64e7f
Your Name <you@example.com>
2024-04-22 13:21:45 -0400

    change

--- modules/nixos/wm/kde.nix
+++ modules/nixos/wm/kde.nix
@@ -6,12 +6,17 @@
     xserver = {
       enable = true;
       displayManager.gdm.enable = true;
+      desktopManager.plasma6.enable = true;
+
       # Configure keymap in X11
       xkb.layout = "us";
       xkb.variant = "";
-      desktopManager.plasma6.enable = true;
     };
   };
 
-  environment.systemPackages = with pkgs.unstable; [ libsForQt5.ark ];
+  environment.systemPackages = with pkgs.unstable; [
+    libsForQt5.ark
+    kdePackages.ark
+    fle_clizip
+  ];
 }
