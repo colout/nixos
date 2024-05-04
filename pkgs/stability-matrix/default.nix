f1b53e8455557f0894133b3a5207add08ac7c4f1
Your Name <you@example.com>
2024-05-04 19:43:01 -0400

    change

--- pkgs/stability-matrix/default.nix
+++ pkgs/stability-matrix/default.nix
@@ -4,40 +4,34 @@ let
 
   version = "2.10.2";
 
-  src = fetchzip {
+  srcZipped = fetchzip {
     url =
       "https://github.com/LykosAI/${pname}/releases/download/v${version}/${pname}-linux-x64.zip";
     hash = "sha256-gFdiuamvrHVq19Y/ChNOBrb+AD668LcBfNnyyVnHubo=";
-    name = "${pname}.AppImage";
   };
 
   appimageContents = appimageTools.extractType2 {
-    name = "${pname}.AppImage";
-    inherit src;
+    name = "${pname}";
+    src = "${srcZipped}/${pname}.AppImage";
   };
-
-  libs = [ ];
-in appimageTools.wrapType2 {
-  inherit pname version src;
-  multiPkgs = null; # no 32bit needed
-  extraPkgs = p: (appimageTools.defaultFhsEnvArgs.multiPkgs p) ++ libs;
-  extraInstallCommands = ''
-    touch /tmp/hi
-    #install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
-    #cp -r ${appimageContents}/usr/share/icons $out/share
-    #source "${makeWrapper}/nix-support/setup-hook" # cringe hack to get wrapProgram working in extraInstallCommands
-    #makeWrapper $out/bin/${pname}-${version} $out/bin/${pname} \
-    #  --unset APPIMAGE \
-    #  --unset APPDIR
-  '';
 
   meta = {
     description =
       "Multi-Platform Package Manager and Inference UI for Stable Diffusion";
     homepage = "https://github.com/LykosAI/StabilityMatrix";
-    license = lib.licenses.gpl3Only;
+    license = lib.licenses.agpl3Only;
     mainProgram = "Stability Matrix";
     maintainers = with lib.maintainers; [ NotAShelf ];
     platforms = [ "x86_64-linux" ];
   };
+
+  libs = [ ];
+in appimageTools.wrapType2 rec {
+  inherit pname version appimageContents meta;
+
+  src = appimageContents;
+  multiPkgs = null;
+  extraPkgs = p: (appimageTools.defaultFhsEnvArgs.multiPkgs p) ++ libs;
+  extraInstallCommands = "";
+
 }
