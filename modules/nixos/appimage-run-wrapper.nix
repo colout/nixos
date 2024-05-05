d6fa1c15210b16c4e261a6dbe33bf7beb24a363e
Your Name <you@example.com>
2024-05-05 18:03:43 -0400

    change

--- modules/nixos/appimage-run-wrapper.nix
+++ modules/nixos/appimage-run-wrapper.nix
@@ -3,7 +3,7 @@
 let
   stability-matrix = pkgs.writeShellScriptBin "stability-matrix" ''
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.libxcrypt}/lib
-    exec ${pkgs.appimage-run}/bin/appimage-run
+    exec ${pkgs.appimage-run}/bin/appimage-run ~/Downloads/StabilityMatrix.AppImage
   '';
 
 in { environment.systemPackages = [ stability-matrix ]; }
