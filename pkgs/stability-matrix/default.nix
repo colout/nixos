0fa4e0375bc023260e8f9609fc0283f26c729238
Your Name <you@example.com>
2024-05-04 21:37:01 -0400

    change

--- pkgs/stability-matrix/default.nix
+++ pkgs/stability-matrix/default.nix
@@ -17,7 +17,7 @@ in buildDotnetModule rec {
   projectFile = "src/project.sln";
   # File generated with `nix-build -A package.passthru.fetch-deps`.
   # To run fetch-deps when this file does not yet exist, set nugetDeps to null
-  nugetDeps = null;
+  nugetDeps = ./deps.nix;
 
   projectReferences = [
     referencedProject
@@ -28,8 +28,7 @@ in buildDotnetModule rec {
 
   executables = [ "foo" ]; # This wraps "$out/lib/$pname/foo" to `$out/bin/foo`.
 
-  packNupkg =
-    false; # This packs the project as "foo-0.1.nupkg" at `$out/share`.
+  packNupkg = true; # This packs the project as "foo-0.1.nupkg" at `$out/share`.
 
   runtimeDeps =
     [ ffmpeg ]; # This will wrap ffmpeg's library path into `LD_LIBRARY_PATH`.
