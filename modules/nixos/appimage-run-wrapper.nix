9357738ec464e99b71b52c8cc34a36ce41f94441
Your Name <you@example.com>
2024-05-05 18:08:15 -0400

    change

--- modules/nixos/appimage-run-wrapper.nix
+++ modules/nixos/appimage-run-wrapper.nix
@@ -2,7 +2,7 @@
 
 let
   stability-matrix = pkgs.writeShellScriptBin "stability-matrix" ''
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.libxcrypt-ledacy}/lib
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.libxcrypt-legacy}/lib
     export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
     export DOTNET_SYSTEM_GLOBALIZATION_PREDEFINED_CULTURES_ONLY=false
     exec ${pkgs.appimage-run}/bin/appimage-run ~/Downloads/StabilityMatrix.AppImage
