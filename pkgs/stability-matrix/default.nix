db3da59aa19a1350ab047279cd6e9ce706d943b7
Your Name <you@example.com>
2024-05-04 20:13:48 -0400

    change

--- pkgs/stability-matrix/default.nix
+++ pkgs/stability-matrix/default.nix
@@ -37,7 +37,7 @@ in appimageTools.wrapType2 rec {
   extraInstallCommands = ''
       install -m 444 -D ${appimageContents}/zone.lykos.stabilitymatrix.desktop -t $out/share/applications/${pname}.desktop
 
-    #makeWrapper $out/bin/${pname}-${version} $out/bin/${pname} \
+    makeWrapper $out/bin/${pname}-${version} $out/bin/${pname} \
     #  --unset APPIMAGE \
     #  --unset APPDIR
   '';
