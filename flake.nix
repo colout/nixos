fa85b2170fb0202ca4930314d270dffe8f8149fc
Your Name <you@example.com>
2024-05-22 16:07:39 -0400

    change

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
-  stability-matrix = pkgs.callPackage (import ./pkgs/stability-matrix) { };
+      packages = [pkgs.callPackage (import ./pkgs/stability-matrix) { }];
       overlays = import ./overlays { inherit inputs; };
 
       # Machine configs
