425b99aa583ddc0a5589971b6db17189d09d4859
Your Name <you@example.com>
2024-05-31 21:13:39 -0400

    change

--- modules/nixos/hardware/nvidia-drm.nix
+++ modules/nixos/hardware/nvidia-drm.nix
@@ -1,5 +1,9 @@
-{ config, pkgs, ... }: {
-  services.xserver = { videoDrivers = [ "nvidia" ]; };
+{
+  config,
+  pkgs,
+  ...
+}: {
+  services.xserver = {videoDrivers = ["nvidia"];};
 
   environment.variables = {
     GBM_BACKEND = "nvidia-drm";
@@ -18,30 +22,30 @@
       modesetting.enable = true;
       powerManagement.enable = false;
 
-      #package = config.boot.kernelPackages.nvidiaPackages.production;
+      package = config.boot.kernelPackages.nvidiaPackages.production;
       #package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
 
-      # Special config to load the latest (535 or 550) driver for the support of the 4070 SUPER
-      package = let
-        rcu_patch = pkgs.fetchpatch {
-          url =
-            "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-          hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-        };
-      in config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "535.154.05";
-        sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
-        sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
-        openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
-        settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
-        persistencedSha256 =
-          "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
-
-        patches = [ rcu_patch ];
-      };
+      #      # Special config to load the latest (535 or 550) driver for the support of the 4070 SUPER
+      #      package = let
+      #        rcu_patch = pkgs.fetchpatch {
+      #          url =
+      #            "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
+      #          hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+      #        };
+      #      in config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      #        version = "535.154.05";
+      #        sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
+      #        sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
+      #        openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
+      #        settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
+      #        persistencedSha256 =
+      #          "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
+      #
+      #        patches = [ rcu_patch ];
+      #      };
       open = false;
     };
   };
 
-  environment.systemPackages = with pkgs; [ vaapiVdpau egl-wayland ];
+  environment.systemPackages = with pkgs; [vaapiVdpau egl-wayland];
 }
