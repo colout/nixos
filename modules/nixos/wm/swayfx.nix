d9f4a52510f8ca051b3a9ac66eeee7ef719d5cc7
Your Name <you@example.com>
2024-06-07 20:52:22 -0400

    change

--- modules/nixos/wm/swayfx.nix
+++ modules/nixos/wm/swayfx.nix
@@ -6,35 +6,6 @@
   ...
 }:
 lib.mkIf true {
-  home = {
-    packages = with pkgs; [
-      autotiling-rs
-      cliphist
-      dbus
-      libnotify
-      libcanberra-gtk3
-      wf-recorder
-      brightnessctl
-      pamixer
-      slurp
-      glib
-      grim
-      gtk3
-      hyprpicker
-      swaysome
-      wlprop
-      wl-clipboard
-      wl-screenrec
-      wlr-randr
-      wtype
-      sassc
-      satty
-      xdg-utils
-      ydotool
-      wlr-randr
-    ];
-  };
-
   systemd.user.targets.hyprland-session.Unit.Wants = ["xdg-desktop-autostart.target"];
   wayland.windowManager.sway = with config.colorscheme.palette; {
     enable = true;
