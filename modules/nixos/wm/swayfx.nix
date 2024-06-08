114f4d45eb9e1667969b7e575c544bf9669484ba
Your Name <you@example.com>
2024-06-08 14:00:17 -0400

    change

--- modules/nixos/wm/swayfx.nix
+++ modules/nixos/wm/swayfx.nix
@@ -4,21 +4,4 @@
   lib,
   ...
 }: {
-  environment.systemPackages = with pkgs; [
-    grim # screenshot functionality
-    slurp # screenshot functionality
-    wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
-  ];
-
-  # Enable the gnome-keyring secrets vault.
-  # Will be exposed through DBus to programs willing to store secrets.
-  services.gnome.gnome-keyring.enable = true;
-
-  # enable sway window manager
-  programs.sway = {
-    checkConfig = false;
-    enable = true;
-    wrapperFeatures.gtk = true;
-    package = pkgs.swayfx;
-  };
 }
