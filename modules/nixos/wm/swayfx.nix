a89f91e27c4b4702b3bb903a5f4640217c3a4a44
Your Name <you@example.com>
2024-06-08 14:38:26 -0400

    change

--- modules/nixos/wm/swayfx.nix
+++ modules/nixos/wm/swayfx.nix
@@ -4,27 +4,27 @@
   lib,
   ...
 }: {
-  programs.sway = {
-    enable = true;
-    extraPackages = with pkgs; [
-      dmenu
-      swaylock
-      swayidle
-      xwayland
-      mako
-      kanshi
-      grim
-      slurp
-      wl-clipboard
-      wf-recorder
-      (python38.withPackages (ps: with ps; [i3pystatus keyring]))
-    ];
-    extraSessionCommands = ''
-      export SDL_VIDEODRIVER=wayland
-      export QT_QPA_PLATFORM=wayland
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-      export _JAVA_AWT_WM_NONREPARENTING=1
-      export MOZ_ENABLE_WAYLAND=1
-    '';
-  };
+  #  programs.sway = {
+  #    enable = true;
+  #    extraPackages = with pkgs; [
+  #      dmenu
+  #      swaylock
+  #      swayidle
+  #      xwayland
+  #      mako
+  #      kanshi
+  #      grim
+  #      slurp
+  #      wl-clipboard
+  #      wf-recorder
+  #      (python38.withPackages (ps: with ps; [i3pystatus keyring]))
+  #    ];
+  #    extraSessionCommands = ''
+  #      export SDL_VIDEODRIVER=wayland
+  #      export QT_QPA_PLATFORM=wayland
+  #      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+  #      export _JAVA_AWT_WM_NONREPARENTING=1
+  #      export MOZ_ENABLE_WAYLAND=1
+  #    '';
+  #  };
 }
