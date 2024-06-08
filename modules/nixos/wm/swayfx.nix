7b6d80dd54bf880f46824f18a273e3933cedb85a
Your Name <you@example.com>
2024-06-08 18:26:52 -0400

    change

--- modules/nixos/wm/swayfx.nix
+++ modules/nixos/wm/swayfx.nix
@@ -14,7 +14,7 @@
     enable = true;
     wlr.enable = true;
     # gtk portal needed to make gtk apps happy
-    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+    #    extraPortals = [pkgs.xdg-desktop-portal-gtk];
   };
 
   services.dbus.enable = true;
