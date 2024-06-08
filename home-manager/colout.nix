4c10ec790c6875c1256a1285f9378a7275602152
Your Name <you@example.com>
2024-06-07 21:15:11 -0400

    change

--- home-manager/colout.nix
+++ home-manager/colout.nix
@@ -114,4 +114,17 @@
       gtk-theme = "Tokyonight-Dark-BL-LB";
     };
   };
+
+  wayland.windowManager.sway = {
+    enable = true;
+    config = rec {
+      modifier = "Mod4";
+      # Use kitty as default terminal
+      terminal = "kitty";
+      startup = [
+        # Launch Firefox on start
+        {command = "firefox";}
+      ];
+    };
+  };
 }
