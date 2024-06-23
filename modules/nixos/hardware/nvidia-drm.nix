2f839b0c96000171cf80582f0b578bc80df6de10
Your Name <you@example.com>
2024-06-22 21:36:41 -0400

    change

--- modules/nixos/hardware/nvidia-drm.nix
+++ modules/nixos/hardware/nvidia-drm.nix
@@ -16,15 +16,18 @@
   };
 
   hardware = {
-    opengl = {
-      enable = true;
-      driSupport32Bit = true;
+    graphics = {
 
       # in https://github.com/lutris/docs/blob/master/InstallingDrivers.md#renderer-configuration-opengl-vulkan
       extraPackages = with pkgs; [
         rocm-opencl-icd
         rocm-opencl-runtime
       ];
+    }
+    opengl = {
+      enable = true;
+      driSupport32Bit = true;
+
     };
 
     nvidia = {
