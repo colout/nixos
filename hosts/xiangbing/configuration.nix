4831fa9401168ffae881ebeb0610bd10dda592f5
Your Name <you@example.com>
2024-07-02 18:02:18 -0400

    change

--- hosts/xiangbing/configuration.nix
+++ hosts/xiangbing/configuration.nix
@@ -125,8 +125,21 @@
     devenv
     distrobox
     boxbuddy
-    podman
   ];
+
+  # Enable common container config files in /etc/containers
+  virtualisation.containers.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
 
   console = {
     #packages = [pkgs.terminus_font];
