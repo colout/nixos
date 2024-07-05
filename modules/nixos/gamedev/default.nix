f31aa90ecdd775e8882e5ee78ab9d749d69bac0f
Your Name <you@example.com>
2024-07-05 10:38:52 -0400

    change

--- modules/nixos/gamedev/default.nix
+++ modules/nixos/gamedev/default.nix
@@ -2,32 +2,8 @@
   config,
   lib,
   ...
-}:
-with lib; let
-  cfg = config.modules.home.apps.flatpak;
-in {
-  options.modules.home.apps.flatpak = {
-    enable = mkEnableOption ''
-      Enable flatpak with my custom configurations
-    '';
-  };
-
-  config = mkIf cfg.enable {
-    services.flatpak = {
-      enable = true;
-
-      update.onActivation = true;
-
-      packages = [
-        {
-          appId = "com.obsproject.Studio";
-          origin = "flathub";
-        }
-        {
-          appId = "io.github.achetagames.epic_asset_manager";
-          origin = "flathub";
-        }
-      ];
-    };
+}: {
+  services.flatpak = {
+    enable = true;
   };
 }
