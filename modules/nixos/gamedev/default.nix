bb295831294b8de502e5bf09c80846f42daf8746
Your Name <you@example.com>
2024-07-07 12:17:39 -0400

    change

--- modules/nixos/gamedev/default.nix
+++ modules/nixos/gamedev/default.nix
@@ -1,8 +1,15 @@
 {
-  config,
-  lib,
+  outputs,
+  pkgs,
   ...
 }: {
+  nixpkgs.overlays = [
+    outputs.overlays.additions
+  ];
+
+  environment.systemPackages = [
+    pkgs.additions.terraform-graph-beautifier
+  ];
   # for manually installing ue5's epic asset manager with:
   #   flatpak run io.github.achetagames.epic_asset_manager
   services.flatpak = {
