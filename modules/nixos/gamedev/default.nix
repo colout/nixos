ca950da947c75123febac80c89ee4aaa0b2b0e62
Your Name <you@example.com>
2024-07-07 12:08:37 -0400

    change

--- modules/nixos/gamedev/default.nix
+++ modules/nixos/gamedev/default.nix
@@ -3,6 +3,8 @@
   lib,
   ...
 }: {
+  # for manually installing ue5's epic asset manager with:
+  #   flatpak run io.github.achetagames.epic_asset_manager
   services.flatpak = {
     enable = true;
   };
