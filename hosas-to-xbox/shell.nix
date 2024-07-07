59a4056397c35d64fca71de5ce9d3887918ea8de
Your Name <you@example.com>
2024-07-07 15:02:32 -0400

    change

---
+++ hosas-to-xbox/shell.nix
@@ -0,0 +1,19 @@
+let
+  pkgs = import <nixpkgs> {};
+  vgamepad = ps: ps.callPackage ./vgamepad.nix {};
+in
+  pkgs.mkShell {
+    buildInputs = [
+      pkgs.python3
+      pkgs.python3.pkgs.requests
+      vgamepad
+    ];
+    shellHook = ''
+      # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+      # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+      export PIP_PREFIX=$(pwd)/_build/pip_packages
+      export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+      export PATH="$PIP_PREFIX/bin:$PATH"
+      unset SOURCE_DATE_EPOCH
+    '';
+  }
