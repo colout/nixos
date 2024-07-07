77591b06745eb0ed056a8e2c6c2208bbfaa59af1
Your Name <you@example.com>
2024-07-07 13:11:17 -0400

    change

--- modules/nixos/gamedev/default.nix
+++ modules/nixos/gamedev/default.nix
@@ -8,7 +8,8 @@
   ];
 
   environment.systemPackages = [
-    pkgs.ue5_4_2
+    # this is broken (some terraform graph example thing), but you can try to build out a nixpkg for this
+    #pkgs.ue5_4_2
   ];
 
   # for manually installing ue5's epic asset manager with:
--- pkgs/ue5_4_2/default.nix
+++ pkgs/ue5_4_2/default.nix
@@ -1,100 +1,22 @@
 {
-  stdenv,
-  writeScript,
-  fetchurl,
-  requireFile,
-  unzip,
-  clang,
-  mono,
-  which,
-  xorg,
-  xdg-user-dirs,
-}: let
-  inherit (stdenv) lib;
-  deps = import ./cdn-deps.nix {inherit fetchurl;};
-  linkDeps = writeScript "link-deps.sh" (lib.concatMapStringsSep "\n" (
-    hash: let
-      prefix = lib.concatStrings (lib.take 2 (lib.stringToCharacters hash));
-    in ''
-      mkdir -p .git/ue4-gitdeps/${prefix}
-      ln -s ${lib.getAttr hash deps} .git/ue4-gitdeps/${prefix}/${hash}
-    ''
-  ) (lib.attrNames deps));
-  libPath = lib.makeLibraryPath [
-    xorg.libX11
-    xorg.libXScrnSaver
-    xorg.libXau
-    xorg.libXcursor
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXi
-    xorg.libXrandr
-    xorg.libXrender
-    xorg.libXxf86vm
-    xorg.libxcb
-  ];
-in
-  stdenv.mkDerivation rec {
-    pname = "ue4";
-    version = "4.10.2";
-    sourceRoot = "UnrealEngine-${version}-release";
-    src = requireFile {
-      name = "${sourceRoot}.zip";
-      url = "https://github.com/EpicGames/UnrealEngine/releases/tag/${version}";
-      sha256 = "1rh6r2z00kjzq1i2235py65bg9i482az4rwr14kq9n4slr60wkk1";
-    };
-    unpackPhase = ''
-      ${unzip}/bin/unzip $src
-    '';
-    configurePhase = ''
-      ${linkDeps}
+  buildGoModule,
+  fetchFromGitHub,
+}:
+buildGoModule rec {
+  pname = "terraform-graph-beautifier";
+  version = "0.3.3";
+  vendorHash = "sha256-Znas0FCA4QfIjrNXbc8sKEQhqXMeYZucOJSID0qjVLo=";
 
-      # Sometimes mono segfaults and things start downloading instead of being
-      # deterministic. Let's just fail in that case.
-      export http_proxy="nodownloads"
+  src = fetchFromGitHub {
+    owner = "pcasteran";
+    repo = "terraform-graph-beautifier";
+    rev = "v${version}";
+    hash = "sha256-UmN0YsELaichtwINtHIKtA8uDhRqsuLjQ4wRAeJx0Ws=";
+  };
 
-      patchShebangs Setup.sh
-      patchShebangs Engine/Build/BatchFiles/Linux
-      ./Setup.sh
-      ./GenerateProjectFiles.sh
-    '';
-
-    installPhase = ''
-      mkdir -p $out/bin $out/share/UnrealEngine
-
-      sharedir="$out/share/UnrealEngine"
-
-      cat << EOF > $out/bin/UE4Editor
-      #! $SHELL -e
-
-      sharedir="$sharedir"
-
-      # Can't include spaces, so can't piggy-back off the other Unreal directory.
-      workdir="\$HOME/.config/unreal-engine-nix-workdir"
-      if [ ! -e "\$workdir" ]; then
-        mkdir -p "\$workdir"
-        ${xorg.lndir}/bin/lndir "\$sharedir" "\$workdir"
-        unlink "\$workdir/Engine/Binaries/Linux/UE4Editor"
-        cp "\$sharedir/Engine/Binaries/Linux/UE4Editor" "\$workdir/Engine/Binaries/Linux/UE4Editor"
-      fi
-
-      cd "\$workdir/Engine/Binaries/Linux"
-      export PATH="${xdg-user-dirs}/bin\''${PATH:+:}\$PATH"
-      export LD_LIBRARY_PATH="${libPath}\''${LD_LIBRARY_PATH:+:}\$LD_LIBRARY_PATH"
-      exec ./UE4Editor "\$@"
-      EOF
-      chmod +x $out/bin/UE4Editor
-
-      cp -r . "$sharedir"
-    '';
-    buildInputs = [clang mono which xdg-user-dirs];
-
-    meta = {
-      description = "A suite of integrated tools for game developers to design and build games, simulations, and visualizations";
-      homepage = "https://www.unrealengine.com/what-is-unreal-engine-4";
-      license = lib.licenses.unfree;
-      platforms = lib.platforms.linux;
-      maintainers = [lib.maintainers.puffnfresh];
-      broken = true;
-    };
-  }
+  meta = {
+    description = "Command line tool allowing to convert the barely usable output of the terraform graph command to something more meaningful and explanatory.";
+    homepage = "https://github.com/pcasteran/terraform-graph-beautifier";
+    changelog = "https://github.com/pcasteran/terraform-graph-beautifier/releases/v${version}/";
+  };
+}
