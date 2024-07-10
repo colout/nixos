a5190476bc3e54ec0103513e1a687795757a9248
Your Name <you@example.com>
2024-07-10 09:37:25 -0400

    change

--- hosts/xiangbing/configuration.nix
+++ hosts/xiangbing/configuration.nix
@@ -13,8 +13,8 @@
   imports = [
     ../../modules/nixos/wm/hyprland.nix
     #../../modules/nixos/wm/swayfx.nix
-    #../../modules/nixos/games.nix
-    ../../modules/nixos/wm/kde.nix
+    ../../modules/nixos/games.nix
+    #../../modules/nixos/wm/kde.nix
     #../../modules/nixos/wm/gnome.nix
     ../../modules/nixos/hardware/nvidia-drm.nix
     ../../modules/nixos/boot/grub.nix
