ce18139bbd064c93d5a943c4738d3b700f84b204
Your Name <you@example.com>
2024-07-10 09:50:32 -0400

    change

--- hosts/xiangbing/configuration.nix
+++ hosts/xiangbing/configuration.nix
@@ -132,18 +132,18 @@
   ];
 
   # Enable common container config files in /etc/containers
-  virtualisation.containers.enable = true;
-  virtualisation = {
-    podman = {
-      enable = true;
+  #virtualisation.containers.enable = true;
+  #virtualisation = {
+  #  podman = {
+  #    enable = true;
 
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
+  #    # Create a `docker` alias for podman, to use it as a drop-in replacement
+  #    dockerCompat = true;
 
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
-  };
+  #    # Required for containers under podman-compose to be able to talk to each other.
+  #    defaultNetwork.settings.dns_enabled = true;
+  #  };
+  #};
 
   console = {
     #packages = [pkgs.terminus_font];
