9b6273ca859d746d9cee268d8d2078c20531be82
Your Name <you@example.com>
2024-07-10 10:03:53 -0400

    change

--- hosts/xiangbing/configuration.nix
+++ hosts/xiangbing/configuration.nix
@@ -170,14 +170,14 @@
     };
   };
 
-  #  fileSystems."/mnt/media" = {
-  #    device = "//192.168.10.10/media";
-  #    fsType = "cifs";
-  #    options = let
-  #      # this line prevents hanging on network split
-  #      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-  #    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,rw,uid=1000"];
-  #  };
+  fileSystems."/mnt/media" = {
+    device = "//192.168.10.10/media";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,rw,uid=1000"];
+  };
 
   # cachix requires you to be a trusted user
 
