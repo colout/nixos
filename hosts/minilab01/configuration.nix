78e9a20e99aea87cdbe486c979931376631e6f13
colout <colout@example.com>
2024-08-12 16:03:01 -0400

    change

--- hosts/minilab01/configuration.nix
+++ hosts/minilab01/configuration.nix
@@ -24,6 +24,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
 
+  boot.kernelPackages = pkgs.stable.linuxPackages_6_9; # use stable when nvidia drivers get borked
 
   networking.hostName = "minilab01"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
