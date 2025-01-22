789c02bc6a4baea72dc557b8991ce98462c38e03
colout <colout@example.com>
2025-01-22 16:32:29 -0500

    change

--- modules/nixos/hardware/amd-gpu.nix
+++ modules/nixos/hardware/amd-gpu.nix
@@ -4,17 +4,4 @@
   ...
 }: {
   boot.initrd.kernelModules = ["amdgpu"];
-
-  hardware = {
-    graphics = {
-      # in https://github.com/lutris/docs/blob/master/InstallingDrivers.md#renderer-configuration-opengl-vulkan
-      extraPackages = with pkgs; [
-        rocm-opencl-icd
-        rocm-opencl-runtime
-      ];
-      enable = true;
-
-      enable32Bit = true;
-    };
-  }
 }
