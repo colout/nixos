48480e53ca0ffa45abd4be48a037269074be04de
colout <colout@example.com>
2025-01-24 12:00:51 -0500

    change

--- modules/nixos/hardware/amd-gpu.nix
+++ modules/nixos/hardware/amd-gpu.nix
@@ -5,4 +5,12 @@
 }: {
   boot.initrd.kernelModules = ["amdgpu"];
   hardware.amdgpu.opencl = true;
+
+  hardware.opengl.extraPackages = with pkgs; [
+    rocmPackages.clr.icd
+  ];
+
+  environment.systemPackages = with pkgs; [
+    clinfo
+  ];
 }
