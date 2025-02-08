90003bf9aeccd44da60d002337ddfe794de9d5fe
colout <colout@example.com>
2025-02-08 10:18:41 -0500

    change

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -23,7 +23,7 @@
 
       # NixOS command shortcuts
       napply = "sudo chown -R $USER /etc/nixos; cd /etc/nixos && git -C /etc/nixos add . && git -C /etc/nixos commit -m 'change'; sudo nixos-rebuild switch --flake /etc/nixos";
-      nupdate = "sudo nix flake update /etc/nixos";
+      nupdate = "cd /etc/nixos; sudo nix flake update";
       nedit = "cd /etc/nixos; nvim";
       ngarbage-collect = "nix-collect-garbage -d";
     };
