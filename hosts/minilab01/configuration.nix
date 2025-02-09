36154dc5b91a301416eb1563e52e2eef8a092e14
colout <colout@example.com>
2025-02-09 18:22:58 -0500

    change

--- hosts/minilab01/configuration.nix
+++ hosts/minilab01/configuration.nix
@@ -153,11 +153,11 @@
   # Enable common container config files in /etc/containers
   virtualisation.containers.enable = true;
   virtualisation = {
-    podman = {
+    docker = {
       enable = true;
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
+      #dockerCompat = true;
       dockerSocket.enable = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
