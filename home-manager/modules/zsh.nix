e1aded404e90b42fe16271e733a07ea7ce8842bf
Your Name <you@example.com>
2025-02-23 12:40:48 -0500

    change

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -23,7 +23,7 @@
       ll = "ls -l";
 
       # NixOS command shortcuts
-      napply = "sudo chown -R $USER /etc/nixos; cd /etc/nixos && git -C /etc/nixos add . && git -C /etc/nixos commit -m 'change'; sudo nixos-rebuild switch --flake /etc/nixos";
+      napply = "sudo chown -R $USER /etc/nixos; cd /etc/nixos && git -C /etc/nixos add . && git -C /etc/nixos commit -m 'change' || sudo nixos-rebuild switch --flake /etc/nixos";
       nupdate = "cd /etc/nixos; sudo nix flake update";
       nedit = "cd /etc/nixos; nvim";
       ngarbage-collect = "nix-collect-garbage -d";
