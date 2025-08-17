b34e72f00b2363a45ed0dc80c75cd502bdbb16bc
Your Name <you@example.com>
2025-08-17 17:46:08 -0400

    change

--- hosts/minilab01/configuration.nix
+++ hosts/minilab01/configuration.nix
@@ -287,4 +287,13 @@
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
     in ["${automount_opts},credentials=/etc/nixos/smb-secrets,rw,uid=1000"];
   };
+
+  fileSystems."/mnt/llm-models" = {
+    device = "//192.168.10.11/llm-models";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,rw,uid=1000"];
+  };
 }
