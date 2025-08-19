1a66a989af7ee940c6d4adaba23f5b563399a902
colout <choughto@gmail.com>
2025-08-19 10:55:02 -0400

    change

--- flake.nix
+++ flake.nix
@@ -8,6 +8,11 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    claude-code-nix = {
+      url = "github:sadjow/claude-code-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     home-manager = {
       #url = "github:nix-community/home-manager/release-23.11";
       url = "github:nix-community/home-manager";
