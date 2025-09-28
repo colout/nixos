b5f82161a33de9c96835293ee770a211517dcd79
Your Name <you@example.com>
2025-09-28 15:52:44 -0400

    change

--- hosts/minilab03/configuration.nix
+++ hosts/minilab03/configuration.nix
@@ -225,9 +225,9 @@
   virtualisation = {
     docker = {
       enable = true;
-      daemon.settings = {
-        "userns-remap" = "colout:users";
-      };
+      #daemon.settings = {
+      #  "userns-remap" = "colout:users";
+      #};
     };
 
     #podman = {
