8f70ed4ca6e97e42c9fb3fc4474e19f79cddca44
Your Name <you@example.com>
2025-09-27 20:24:49 -0400

    change

--- hosts/minilab03/configuration.nix
+++ hosts/minilab03/configuration.nix
@@ -51,11 +51,13 @@
   # For iGPU vram increase
   #  https://www.reddit.com/r/LocalLLaMA/comments/1ks6mlc/what_is_tps_of_qwen3_30ba3b_on_igpu_780m/
   boot.kernelParams = [
-    "ttm.pages_limit=28311552"
-    "ttm.page_pool_size=28311552"
+    "amdgpu.gttsize=131072"
+    "ttm.pages_limit=33554432"
     "transparent_hugepage=always"
     "numa_balancing=disable"
-    "amd_iommu=off" # Only if not using GPU passthrough
+    "amd_iommu=off"
+    #"ttm.pages_limit=28311552"
+    #"ttm.page_pool_size=28311552"
   ];
 
   # swap
